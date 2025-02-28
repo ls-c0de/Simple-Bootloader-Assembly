bba4e0201cdb80cce8eb3326cda6223fc05ecf0d
zweideutigkeit <testmail@test.de>
2025-02-28 14:31:35 +0100

    boot is now printing keys

--- boot.asm
+++ boot.asm
@@ -1,23 +1,18 @@
 [BITS 16]
 [ORG 0x7C00]  ; BIOS lädt Bootloader an Adresse 0x7C00
 
-	mov si, msg
-	call print_string
-
+	call key_return
 	jmp $
 
-print_string:
-        mov ah, 0x0E  ; BIOS-Teletype Mode für Textausgabe
-.loop:
-	lodsb         ; Nächstes Zeichen aus SI laden
-	cmp al, 0
-	je .done
-	int 0x10      ; BIOS-Interrupt für Zeichenanzeige
-	jmp .loop
-.done:
-	ret
+print_key:
+	mov ah, 0x0E
+	int 0x10
 
-msg db "Hello, Bootloader!", 0
+key_return:
+	mov ah, 0x00
+	int 0x16
+	call print_key
+	
 
 times 510-($-$$) db 0  ; Auffüllen bis 510 Bytes
 dw 0xAA55  ; Boot-Signatur
